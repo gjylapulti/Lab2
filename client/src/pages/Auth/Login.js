a37e08d60d8970f26f7de5c57398b577630ca20c
gjylapulti <gp48517@ubt-uni.net>
2023-10-18 17:07:44 +0200

    context api

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -4,10 +4,12 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
+import { useAuth } from "../../context/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
   const navigate = useNavigate();
 
   //form function
@@ -20,9 +22,17 @@ const Login = () => {
       });
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
+
+        localStorage.setItem("auth", JSON.stringify(res.data));
+
         setTimeout(() => {
           navigate("/");
-        }, 2300); // Delay in milliseconds (e.g., 2000ms = 2 seconds)
+        }, 1800); // Delay in milliseconds (e.g., 2000ms = 2 seconds)
       } else {
         toast.error(res.data.message);
       }
