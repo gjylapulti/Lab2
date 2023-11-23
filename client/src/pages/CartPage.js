44d55f891198c800a9fd9a68a6d156759dba2b4d
gjylapulti <gp48517@ubt-uni.net>
2023-11-23 11:02:00 +0100

    Added User and Admin orders

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -168,9 +168,6 @@ const CartPage = () => {
                   <DropIn
                     options={{
                       authorization: clientToken,
-                      paypal: {
-                        flow: "vault",
-                      },
                     }}
                     onInstance={(instance) => setInstance(instance)}
                   />
